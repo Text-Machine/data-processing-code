e97fdb68511f48ebfd78a8a3530dde7c5fc17a15
marangiop <marangiopaolo@gmail.com>
2026-06-04 07:26:45 +0200

    [TM-44][TM-62] fix naming inconsistencies in env and launch file

--- llm_genre_classifier_blmicrosoft/launch_genre_classifier.sh
+++ llm_genre_classifier_blmicrosoft/launch_genre_classifier.sh
@@ -5,8 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=80
 #SBATCH --gres=gpu:4
-##SBATCH --mem=32G
-#SBATCH --time=00:40:00
+#SBATCH --time=00:20:00
 #SBATCH --account=bsc100
 ##SBATCH --qos=acc_bsccssh
 #SBATCH --qos=acc_debug
@@ -21,12 +20,12 @@ mkdir -p "$LOG_DIR"
 
 #export HF_HOME=/gpfs/scratch/$USER/.cache/huggingface
 export HF_HOME=/gpfs/scratch/bsc100/paolo/.cache/huggingface
-ENV_PATH="/gpfs/scratch/bsc100/paolo/.conda/envs/textmachine-llm-v4"
+ENV_PATH="/gpfs/scratch/bsc100/paolo/.conda/envs/textmachine-llm"
 
 # ---------------------------------------------------------------------------
 # Paths
 # ---------------------------------------------------------------------------
-SCRIPT="$SLURM_SUBMIT_DIR/genre_classifier_v9.py"
+SCRIPT="$SLURM_SUBMIT_DIR/genre_classifier.py"
 OUTPUT_DIR="/gpfs/scratch/bsc100/paolo/llm_genre_classifier_results"
 mkdir -p "$OUTPUT_DIR"
 
